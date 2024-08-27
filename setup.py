053eef168554001e65a7d2bfc18b0982daf73a3c
Giuseppe Lo Presti <giuseppe.lopresti@cern.ch>
2024-08-27 18:04:55 +0200

    packaging: use dynamic version from CI

--- setup.py
+++ setup.py
@@ -9,12 +9,16 @@ Last updated: 26/07/2024
 """
 
 from setuptools import setup, find_packages
+import os
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+version = os.getenv("PACKAGE_VERSION", "0.0.0")
+
 setup(
     name="cs3client",
+    version=version,
     author="Rasmus Welander, Diogo Castro, Giuseppe Lo Presti",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
